b10d23409163758cc725e7cfaf8bc50a28adbe08
alfonzlazri@yea.al <alfonzlazri@yea.al>
2024-08-27 18:44:55 +0200

    testimonials

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -5,34 +5,34 @@ import { Testimonial } from "@/types/testimonial";
 export const testimonialData: Testimonial[] = [
   {
     id: 1,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "Elira Gashi",
+    designation: "",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "SnapJob ka ndryshuar mënyrën si gjej punë. Gjetëm mundësi të shkëlqyera për punë të përditshme dhe me orar të pjesshëm, dhe është shumë e lehtë të publikoni ofertat e punës.",
   },
   {
     id: 2,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Ardit Berisha",
+    designation: "",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Kam përdorur SnapJob për të gjetur një punë të përkohshme dhe kam qenë shumë i kënaqur me shërbimin. Platforma është e thjeshtë për t'u naviguar dhe ofron mundësi të shumta.",
   },
   {
     id: 3,
-    name: "Devid Smith",
-    designation: "Founter @democompany",
+    name: "Mira Koliqi",
+    designation: "",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "SnapJob është një zgjidhje e shkëlqyer për ata që kërkojnë mundësi pune të përditshme ose me orar të pjesshëm. Shërbimi është efikas dhe ndihmon shumë në gjetjen e punës së duhur.",
   },
   {
     id: 4,
-    name: "Jhon Abraham",
-    designation: "Founter @democompany",
+    name: "Luan Hoxha",
+    designation: "",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Përvoja ime me SnapJob ka qenë fantastike. Kam mundur të publikoj ofertat e punës për kompaninë time dhe gjithashtu kam gjetur kandidatë të kualifikuar për pozitat që kishim hapur.",
   },
 ];
